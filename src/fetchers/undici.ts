51f0db521470b0c5d2fb06d61969052cf2136b2b
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-04-10 01:50:49 +0200

    feat: add undici fetcher

---
+++ src/fetchers/undici.ts
@@ -0,0 +1,26 @@
+import { fetch as _fetch } from "undici/lib/web/fetch/index.js";
+//                               ^ import from path to avoid bun overriding it.
+
+// We have to type the fetch function since no types are provided for it.
+const fetch = _fetch as typeof globalThis.fetch;
+
+import type { SchwiFetcher } from "../schwi";
+import { HeaderMap } from "../headers";
+
+export const fetcher: SchwiFetcher = async (req) => {
+  const response = await fetch(req.url.href, {
+    redirect: req.redirect ?? "follow",
+    headers: req.headers ?? {},
+    method: req.method ?? "GET",
+    body: req.content,
+
+    // We don't want to send cookies, only the ones we set manually.
+    credentials: "omit"
+  });
+
+  return {
+    status: response.status,
+    content: await response.text(),
+    headers: new HeaderMap(response.headers)
+  };
+};
