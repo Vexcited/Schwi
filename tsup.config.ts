6820375335db84bcc7c694b82e6db34cffd96894
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-04-10 22:25:02 +0200

    chore: tsup build

---
+++ tsup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  clean: true,
+  dts: true,
+  entry: [
+    "src/index.react-native.ts",
+    "src/index.tauri.ts",
+    "src/index.node.ts",
+    "src/index.bun.ts",
+  ],
+  format: ["cjs", "esm"],
+  minify: "terser",
+  outDir: "dist",
+  sourcemap: true,
+  splitting: false,
+  treeshake: true
+});
