a11dde5e00857efacbb7944ea8d86120f87c80db
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-04-15 16:44:00 +0200

    feat: try to write a new response class

--- src/response.ts
+++ src/response.ts
@@ -1,5 +1,16 @@
-export class HttpResponse {
-  public static Builder = class HttpResponseBuilder {
+import type { HeaderMap } from "./headers"
 
+export class HttpResponse {
+  constructor (
+    public url: string,
+    public status: number,
+    public headers: HeaderMap,
+    public toString: () => Promise<string>,
+    public toArrayBuffer: () => Promise<ArrayBuffer>,
+  ) {}
+
+  public async toJSON <T = unknown> (): Promise<T> {
+    const string = await this.toString();
+    return JSON.parse(string) as T;
   }
 }
