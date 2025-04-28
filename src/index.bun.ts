285a0a6a75ef1442a4f706c0e0b8a743c9dff91e
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-04-28 17:36:08 +0200

    feat: add `toHTML` response method

--- src/index.bun.ts
+++ src/index.bun.ts
@@ -5,5 +5,6 @@ export * from "./form";
 export { HeaderKeys, HeaderMap } from "./headers";
 export { HttpRequest } from "./request";
 export { HttpResponse } from "./response";
+export type { CheerioAPI } from "cheerio";
 
 export const send = factory(fetcher);
--- src/response.ts
+++ src/response.ts
@@ -1,3 +1,4 @@
+import type { CheerioAPI } from "cheerio";
 import type { HeaderMap } from "./headers";
 
 export class HttpResponse {
@@ -8,6 +9,19 @@ export class HttpResponse {
     public toString: () => Promise<string>,
     public toArrayBuffer: () => Promise<ArrayBuffer>
   ) {}
+
+  /**
+   * Uses `cheerio` under the hood to parse the HTML response
+   * and return a `CheerioAPI` instance.
+   */
+  public async toHTML(): Promise<CheerioAPI> {
+    const html = await this.toString();
+
+    // Dynamically import cheerio to avoid bundling it with the rest of the code.
+    const cheerio = await import("cheerio");
+
+    return cheerio.load(html);
+  }
 
   public async toJSON <T = unknown>(): Promise<T> {
     const string = await this.toString();
