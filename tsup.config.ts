1272bccfc355dd7840a49f4f4d37f73cf3f97105
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-04-29 02:15:37 +0200

    fix: compilation options

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,18 +1,49 @@
+import type { Options } from "tsup";
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  clean: true,
+const defaults: Options = {
   dts: true,
-  entry: [
-    "src/index.react-native.ts",
-    "src/index.tauri.ts",
-    "src/index.node.ts",
-    "src/index.bun.ts"
-  ],
+  external: ["cheerio"],
   format: ["cjs", "esm"],
   minify: "terser",
   outDir: "dist",
   sourcemap: true,
-  splitting: false,
   treeshake: true
-});
+};
+
+export default defineConfig([
+  {
+    ...defaults,
+    clean: true, // Only on the first build !
+    entry: [
+      "src/index.node.ts"
+    ]
+  },
+  {
+    ...defaults,
+    entry: [
+      "src/index.bun.ts"
+    ],
+    external: [
+      ...<string[]>defaults.external,
+      "undici"
+    ]
+  },
+  {
+    ...defaults,
+    entry: [
+      "src/index.tauri.ts"
+    ],
+    external: [
+      ...<string[]>defaults.external,
+      "@tauri-apps/plugin-http"
+    ]
+  },
+  {
+    ...defaults,
+    entry: [
+      "src/index.react-native.ts"
+    ],
+    target: "es6" // Make sure React Native supports the library.
+  }
+]);
